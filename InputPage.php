dbbd7a1325908df54463d379f6ebec83ad514ee4
Hasan Sayfi <hasan_007_h@hotmail.com>
2020-02-15 17:45:43 +0300

    finished input and output hwk

--- InputPage.php
+++ InputPage.php
@@ -12,79 +12,102 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="bootstrap-4.4.1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
     <script>
+        // Material Select Initialization
+        $(document).ready(function() {
+            $('.mdb-select').materialSelect();
+        });
     </script>
+
+    <style>
+        .headLabel{
+            font-size: 14px;
+            font-weight: bolder;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
 
-        <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div id="signupbox" style=" margin-top:100px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
             <div class="panel panel-info">
                 <div class="panel-heading" style="text-align: center;">
-                    <div class="panel-title">Grades Input</div>
+                    <div class="panel-title" style="font-size: 24px; font-weight: bolder;">Grades Input</div>
                 </div>
                 <div class="panel-body">
-                    <form method="post" action=".">
-                        <input type='hidden' name='csrfmiddlewaretoken' value='XFe2rTYl9WOpV8U6X5CfbIuOZOELJ97S' />
-
-
-                        <form class="form-horizontal" method="post">
-
-
-                            <div id="div_id_name" class="form-group required">
-                                <label for="id_name" class="control-label col-md-4  requiredField">Student Name<span class="asteriskField">*</span> </label>
-                                <div class="controls col-md-8 ">
-                                    <input class="input-md textinput textInput form-control" id="id_name" name="name" placeholder="Your Frist name and Last name" style="margin-bottom: 10px" type="text" />
+                    <form action="outputpage.php" method="POST" class="form-horizontal">
+                        <div style="margin-left: 100px;">
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="studentName">Student Name</label>
+                                    <input type="text" class="form-control" id="studentName" name="studentName" placeholder="Student Name" required>
                                 </div>
                             </div>
-                            <div class="form-group col-md-4">
-                                <label for="regState">Grades</label>
-                                <select class="mdb-select md-form">
-                                    <option value="" disabled selected>Choose your option</option>
-                                    <option value="1">Option 1</option>
-                                    <option value="2">Option 2</option>
-                                    <option value="3">Option 3</option>
-                                </select>
-                            </div>
-                            <div id="div_id_company" class="form-group required">
-                                <label for="id_company" class="control-label col-md-4  requiredField">Subject<span class="asteriskField">*</span> </label>
-                                <div class="controls col-md-8 ">
-                                    <input class="input-md textinput textInput form-control" id="id_company" name="company" placeholder="your company name" style="margin-bottom: 10px" type="text" />
+
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="major">Major</label>
+                                    <select id="major" class="form-control" name="major" required>
+                                        <option value="" disabled selected>Choose Major ....</option>
+                                        <option value="Software Engineer">Software Engineer</option>
+                                        <option value="Network Engineer">Network Engineer</option>
+                                        <option value="Science">Science</option>
+                                        <option value="Mathmatecs">Mathmatecs</option>
+                                    </select>
                                 </div>
                             </div>
-                            <div id="div_id_catagory" class="form-group required">
-                                <label for="id_catagory" class="control-label col-md-4  requiredField">Grade 1<span class="asteriskField">*</span> </label>
-                                <div class="controls col-md-8 ">
-                                    <input class="input-md textinput textInput form-control" id="id_catagory" name="catagory" placeholder="skills catagory" style="margin-bottom: 10px" type="text" />
+
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="subject">Subject</label>
+                                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
                                 </div>
                             </div>
-                            <div id="div_id_number" class="form-group required">
-                                <label for="id_number" class="control-label col-md-4  requiredField">Grade 2<span class="asteriskField">*</span> </label>
-                                <div class="controls col-md-8 ">
-                                    <input class="input-md textinput textInput form-control" id="id_number" name="number" placeholder="provide your number" style="margin-bottom: 10px" type="text" />
-                                </div>
-                            </div>
-                            <div id="div_id_location" class="form-group required">
-                                <label for="id_location" class="control-label col-md-4  requiredField">Grade 3<span class="asteriskField">*</span> </label>
-                                <div class="controls col-md-8 ">
-                                    <input class="input-md textinput textInput form-control" id="id_location" name="location" placeholder="Your Pincode and City" style="margin-bottom: 10px" type="text" />
+
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="grades">Grades</label>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="controls col-md-8" style="width: 100%;">
-                                    <br />
-                                    <input type="submit" name="sumitData" value="Submit" style="width: 100%;" class="btn btn-primary btn btn-info" />
+                                <div class="form-group col-md-10">
+                                    <div class="form-row col-md-4">
+                                        <label for="exam1">Exam 1</label>
+                                        <input type="number" class="form-control" id="exam1" name="exam1" placeholder="Exam 1" min="0" max="20" required>
+                                    </div>
+                                    <div class="form-row col-md-4">
+                                        <label for="exam2">Exam 2</label>
+                                        <input type="number" class="form-control" id="exam2" name="exam2" placeholder="Exam 2" min="0" max="20" required>
+                                    </div>
+                                    <div class="form-row col-md-4">
+                                        <label for="exam3">Final Exam 3</label>
+                                        <input type="number" class="form-control" id="exam3" name="exam3" placeholder="Exam 3" min="0" max="60" required> 
+                                    </div>
                                 </div>
                             </div>
-
-                        </form>
-
+                        </div>
+                        <div class="form-group col-md-12" style="margin-left: 0px;">
+                            <input type="submit" name="sumitData" value="Submit" style="width: 100%; padding: 10px;" class="btn btn-primary btn btn-info" />
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <script src="bootstrap-4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="bootstrap-4.4.1/site/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')
+    </script>
 </body>
 
 </html>
---
+++ OutputPage.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
+
+    <title>Output Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="bootstrap-4.4.1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+    <script>
+        // Material Select Initialization
+        $(document).ready(function() {
+            $('.mdb-select').materialSelect();
+        });
+    </script>
+
+
+    <style>
+        .headLabel {
+            font-size: 14px;
+            font-weight: bolder;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+
+        <div id="signupbox" style=" margin-top:100px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info">
+                <?php
+                    $studentName = $_REQUEST['studentName'];
+                    $major = $_REQUEST['major'];
+                    $subject = $_REQUEST['subject'];
+                    $exam1 = $_REQUEST['exam1'];
+                    $exam2 = $_REQUEST['exam2'];
+                    $exam3 = $_REQUEST['exam3'];
+                    $grade = $exam1 + $exam2 + $exam3;
+
+                    if ($grade > 89) {
+                        $grade = "A";
+                    }
+                    elseif ($grade > 79) {
+                        $grade = "B";
+                    }
+                    elseif ($grade > 69) {
+                        $grade = "C";
+                    }
+                    elseif ($grade > 59) {
+                        $grade = "D";
+                    }
+                    else {
+                        $grade = "F";
+                    }
+                ?>
+                <div class="panel-heading" style="text-align: center;">
+                    <div class="panel-title" style="font-size: 24px; font-weight: bolder;">Grades Output</div>
+                </div>
+                <div class="panel-body">
+                    <form action="inputpage.php" method="POST" class="form-horizontal">
+                        <div style="margin-left: 100px;">
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="studentName">Student Name</label>
+                                    <input type="text" class="form-control" value="<?php echo $studentName ?>" id="studentName" name="studentName" disabled>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="major">Major</label>
+                                    <input type="text" class="form-control" value="<?php echo $major ?>"  id="major" name="major" disabled>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="subject">Subject</label>
+                                    <input type="text" class="form-control" value="<?php echo $subject ?>"  id="subject" name="subject" disabled>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="form-group col-md-10">
+                                    <label class="headLabel" for="grades">Grade</label>
+                                    <input type="text" class="form-control"  value="<?php echo $grade ?>"  id="major" name="major" disabled>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                        <div class="form-group col-md-12" style="margin-left: 0px;">
+                            <input type="submit" name="sumitData" value="Back" style="width: 100%; padding: 10px;" class="btn btn-primary btn btn-info" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap-4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="bootstrap-4.4.1/site/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')
+    </script>
+</body>
+
+</html>
